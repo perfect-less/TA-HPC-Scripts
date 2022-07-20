412b5a7f237fcc23ca126c93aa8db0c54e4390b7
ikhwansh <ikhwansyatricha@gmail.com>
2022-07-20 11:36:41 +0700

    Fix tuner definitions and add minimal with only 1 lstm layers

--- hpcscripts/trainers/tunerdefinitions.py
+++ hpcscripts/trainers/tunerdefinitions.py
@@ -42,7 +42,7 @@ def default_tuner():
             model.add(
                 keras.layers.Dense(
                     # Tune number of units separately.
-                    units=hp.Int(f"units_{i}", min_value=20, max_value=20, step=100),
+                    units=hp.Int(f"units_{i}", min_value=20, max_value=100, step=20),
                     activation= 'relu' # hp.Choice("activation", ["relu", "tanh"]),
                 )
             )
@@ -81,8 +81,82 @@ def default_tuner():
         label_shift         = 1,
         feature_columns    = [
                                 'hralt_m', 'hdot_1_mps', 'theta_rad', 'cas_mps', 
-                                'gamma_error_rad', 'tailwind_mps', # 'crosswind_mps'
-                                'flap_4_bool', 'flap_5_bool', 'flap_6_bool'
+                                'gamma_error_rad', 'tailwind_mps', # 'crosswind_mps',
+                                'flap_4_bool', 'flap_5_bool', 'flap_6_bool',
+                                'N1s_rpm',
+                            ],
+        seq_labels         = ['elv_l_rad', 'N1s_rpm'],
+        use_residual_wrap  = False,
+
+        model=model_builder
+    )
+
+def minimal_tuner():
+    
+    input_window_width = 10
+
+    def model_builder(hp):
+        model = keras.Sequential()
+
+        lstm_layers = hp.Int("lstm_layers", 0, 1)
+        is_bidirectional = hp.Boolean("is_bidirectional")
+
+        if lstm_layers > 0:
+            for i in range (lstm_layers):
+                ret_seq = not (i == (lstm_layers - 1))
+                if is_bidirectional:
+                    model.add(
+                        tf.keras.layers.Bidirectional(
+                            tf.keras.layers.LSTM(
+                                hp.Int(f"lstm_units_{i}", min_value=16, max_value=128, step=16),
+                                return_sequences=ret_seq
+                            )
+                        )
+                    )
+                else:
+                    model.add(
+                        tf.keras.layers.LSTM(
+                            hp.Int(f"lstm_units_{i}", min_value=16, max_value=128, step=16),
+                            return_sequences=ret_seq
+                        )
+                    )
+        else:
+            model.add(tf.keras.layers.Flatten())
+        
+        # Tune the number of layers.
+        for i in range(hp.Int("num_layers", 1, 2)):
+            model.add(
+                keras.layers.Dense(
+                    # Tune number of units separately.
+                    units=hp.Int(f"units_{i}", min_value=10, max_value=100, step=10),
+                    activation= 'relu' # hp.Choice("activation", ["relu", "tanh"]),
+                )
+            )
+
+        # Add output layer
+        model.add(keras.layers.Dense(
+                        units=len(G_PARAMS.SEQUENTIAL_LABELS)*G_PARAMS.LABEL_WINDOW_WIDTH,
+                        kernel_initializer=tf.initializers.zeros()
+                ))
+        model.add(keras.layers.Reshape([G_PARAMS.LABEL_WINDOW_WIDTH, len (G_PARAMS.SEQUENTIAL_LABELS)]))
+
+        # Compile our model
+        model.compile(
+            optimizer=G_PARAMS.OPTIMIZER,
+            loss=G_PARAMS.LOSS,
+            metrics=G_PARAMS.METRICS
+        )        
+        return model
+
+    return ModelDefBuilder(
+        input_window_width  = input_window_width,
+        label_window_width  = 1,
+        label_shift         = 1,
+        feature_columns    = [
+                                'hralt_m', 'hdot_1_mps', 'theta_rad', 'cas_mps', 
+                                'gamma_error_rad', 'tailwind_mps', # 'crosswind_mps',
+                                'flap_4_bool', 'flap_5_bool', 'flap_6_bool',
+                                'N1s_rpm',
                             ],
         seq_labels         = ['elv_l_rad', 'N1s_rpm'],
         use_residual_wrap  = False,
@@ -94,4 +168,5 @@ def default_tuner():
 
 TUNER_DEFINITIONS = {
     'default' : default_tuner,
+    'minimal' : minimal_tuner,
 }
