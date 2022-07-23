8e5c67361eb5d17f051f84c7b9e4ab8728e4cebc
ikhwansh <ikhwansyatricha@gmail.com>
2022-07-23 08:04:18 +0700

    Add conv and mixed tuner definitions

--- hpcscripts/trainers/tunerdefinitions.py
+++ hpcscripts/trainers/tunerdefinitions.py
@@ -5,7 +5,14 @@ import tensorflow.keras as keras
 from hpcscripts.trainers.modeldefinitions import ModelDefBuilder, ResidualWrapper
 from hpcscripts.option import globalparams as G_PARAMS
 
-def default_tuner():
+FEATURES_COLUMNS = [
+            'hralt_m', 'hdot_1_mps', 'theta_rad', 'cas_mps', 
+            'gamma_error_rad', 'tailwind_mps', 'g_err_d_rad', # 'crosswind_mps',
+            'flap_4_bool', 'flap_5_bool', 'flap_6_bool',
+            # 'N1s_rpm',
+        ]
+
+def lstm_tuner():
     
     input_window_width = 10
 
@@ -79,12 +86,7 @@ def default_tuner():
         input_window_width  = input_window_width,
         label_window_width  = 1,
         label_shift         = 1,
-        feature_columns    = [
-                                'hralt_m', 'hdot_1_mps', 'theta_rad', 'cas_mps', 
-                                'gamma_error_rad', 'tailwind_mps', # 'crosswind_mps',
-                                'flap_4_bool', 'flap_5_bool', 'flap_6_bool',
-                                # 'N1s_rpm',
-                            ],
+        feature_columns    = FEATURES_COLUMNS,
         seq_labels         = ['elv_l_rad', 'N1s_rpm'],
         use_residual_wrap  = False,
 
@@ -152,12 +154,136 @@ def minimal_tuner():
         input_window_width  = input_window_width,
         label_window_width  = 1,
         label_shift         = 1,
-        feature_columns    = [
-                                'hralt_m', 'hdot_1_mps', 'theta_rad', 'cas_mps', 
-                                'gamma_error_rad', 'tailwind_mps', # 'crosswind_mps',
-                                'flap_4_bool', 'flap_5_bool', 'flap_6_bool',
-                                # 'N1s_rpm',
-                            ],
+        feature_columns    = FEATURES_COLUMNS,
+        seq_labels         = ['elv_l_rad', 'N1s_rpm'],
+        use_residual_wrap  = False,
+
+        model=model_builder
+    )
+
+def conv_tuner():
+    
+    input_window_width = 10
+
+    def model_builder(hp):
+        model = keras.Sequential()
+
+        conv_layers = hp.Int("conv_layers", 1, 2)
+
+        if conv_layers > 0:
+            for i in range (conv_layers):
+                ret_seq = not (i == (conv_layers - 1))
+                model.add(
+                    tf.keras.layers.Conv1D(
+                        hp.Int(f"conv_filters_{i}", min_value=16, max_value=128, step=16),
+                        kernel_size=hp.Int(f"conv_kernel_{i}", min_value=3, max_value=8, step=1),
+                        activation='relu'                        
+                    )
+                )
+        else:
+            model.add(tf.keras.layers.Flatten())
+        
+        # Tune the number of layers.
+        for i in range(hp.Int("num_layers", 1, 2)):
+            model.add(
+                keras.layers.Dense(
+                    # Tune number of units separately.
+                    units=hp.Int(f"units_{i}", min_value=10, max_value=100, step=10),
+                    activation= 'relu' # hp.Choice("activation", ["relu", "tanh"]),
+                )
+            )
+
+        # Add output layer
+        model.add(keras.layers.Dense(
+                        units=len(G_PARAMS.SEQUENTIAL_LABELS)*G_PARAMS.LABEL_WINDOW_WIDTH,
+                        kernel_initializer=tf.initializers.zeros()
+                ))
+        model.add(keras.layers.Reshape([G_PARAMS.LABEL_WINDOW_WIDTH, len (G_PARAMS.SEQUENTIAL_LABELS)]))
+
+        # Compile our model
+        model.compile(
+            optimizer=G_PARAMS.OPTIMIZER,
+            loss=G_PARAMS.LOSS,
+            metrics=G_PARAMS.METRICS
+        )        
+        return model
+
+    return ModelDefBuilder(
+        input_window_width  = input_window_width,
+        label_window_width  = 1,
+        label_shift         = 1,
+        feature_columns    = FEATURES_COLUMNS,
+        seq_labels         = ['elv_l_rad', 'N1s_rpm'],
+        use_residual_wrap  = False,
+
+        model=model_builder
+    )
+
+def mixed_tuner():
+    
+    input_window_width = 10
+
+    def model_builder(hp):
+        model = keras.Sequential()
+
+        early_layers = hp.Int("early_layers", 1, 2)
+        padd = 'valid'
+
+        if early_layers > 0:
+            for i in range (early_layers):
+                kind = hp.Choice("early_layer_{i}", ["lstm", "conv1d"])
+                ret_seq = not (i == (early_layers - 1))
+                if ret_seq:
+                    padd = 'same'
+                if kind == 'conv1d':
+                    model.add(
+                        tf.keras.layers.Conv1D(
+                            hp.Int(f"conv_filters_{i}", min_value=16, max_value=128, step=16),
+                            kernel_size=hp.Int(f"conv_kernel_{i}", min_value=3, max_value=8, step=1),
+                            activation='relu',
+                            padding=padd             
+                        )
+                    )
+                else:
+                    tf.keras.layers.Bidirectional(
+                            tf.keras.layers.LSTM(
+                                hp.Int(f"lstm_units_{i}", min_value=16, max_value=128, step=16),
+                                return_sequences=ret_seq
+                            )
+                        )
+        else:
+            model.add(tf.keras.layers.Flatten())
+        
+        # Tune the number of layers.
+        for i in range(hp.Int("num_layers", 1, 2)):
+            model.add(
+                keras.layers.Dense(
+                    # Tune number of units separately.
+                    units=hp.Int(f"units_{i}", min_value=10, max_value=100, step=10),
+                    activation= 'relu' # hp.Choice("activation", ["relu", "tanh"]),
+                )
+            )
+
+        # Add output layer
+        model.add(keras.layers.Dense(
+                        units=len(G_PARAMS.SEQUENTIAL_LABELS)*G_PARAMS.LABEL_WINDOW_WIDTH,
+                        kernel_initializer=tf.initializers.zeros()
+                ))
+        model.add(keras.layers.Reshape([G_PARAMS.LABEL_WINDOW_WIDTH, len (G_PARAMS.SEQUENTIAL_LABELS)]))
+
+        # Compile our model
+        model.compile(
+            optimizer=G_PARAMS.OPTIMIZER,
+            loss=G_PARAMS.LOSS,
+            metrics=G_PARAMS.METRICS
+        )        
+        return model
+
+    return ModelDefBuilder(
+        input_window_width  = input_window_width,
+        label_window_width  = 1,
+        label_shift         = 1,
+        feature_columns    = FEATURES_COLUMNS,
         seq_labels         = ['elv_l_rad', 'N1s_rpm'],
         use_residual_wrap  = False,
 
@@ -165,8 +291,10 @@ def minimal_tuner():
     )
 
 
-
 TUNER_DEFINITIONS = {
-    'default' : default_tuner,
+    'default' : minimal_tuner,
     'minimal' : minimal_tuner,
+    'lstm'    : lstm_tuner,
+    'conv'    : conv_tuner,
+    'mixed'   : mixed_tuner,
 }
